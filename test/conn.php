aaec3750abd272c7c9e3fbd20c19d26eddb0a506
LucBRT <lucbertin@live.fr>
2017-11-29 09:36:55 +0100

    Update conn.php

--- test/conn.php
+++ test/conn.php
@@ -1,35 +1,23 @@
 <?php 
- define('DB_HOST', 'mysql://mysql-campuslife.alwaysdata.net/');
- define('DB_USER', ‘138846’);
- define('DB_PASS', ‘root’);
- define('DB_NAME', 'campuslife_mabase');
  
- $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
- 
- if (mysqli_connect_errno()) {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
- die();
- }
- 
-$stmt = $conn->prepare("SELECT nom, date, image, description FROM evenements;");
- 
-$stmt->execute();
- 
- //binding results to the query 
- $stmt->bind_result($nom, $date, $image, $description);
- $events = array(); 
- 
- //traversing through all the result 
- while($stmt->fetch()){
- $temp = array();
- $temp[‘nom’] = $nom; 
- /*$temp[‘date’] = $date; 
- $temp[‘image’] = $image; 
- $temp[‘description’] = $description; */
- array_push($events, $temp);
- }
- 
- //displaying the result in json format 
- echo json_encode($events);
 
- ?>
+/* Database config */
+$db_host		= 'mysql-campuslife.alwaysdata.net ';
+$db_user		= '138846';
+$db_pass		= 'root';
+$db_database	= 'campuslife_mabase'; 
+/* End config */
+
+/*
+$db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+*/
+// Create connection
+$conn = new mysqli($db_host, $db_user, $db_pass);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
